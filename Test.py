6b22605b81bcd435ad9768ed4556b53634f7de6f
OMKAR <omkaratanure100@gmail.com>
2026-06-24 19:47:01 +0530

    comited all files

--- Test.py
+++ Test.py
@@ -1,76 +1,25 @@
-def dailog_accept(dailog):
-    dailog.accept()
-
-def dismisss_dailog(dailog):
-    dailog.dismiss()
-
-def prompt_dailog(dailog):
-    dailog.accept("omkarataniure")
-
-
-page.once("dailog", dailog_accept())
-
-
-
 from playwright.sync_api import sync_playwright
 
-def handle_dailg(dialog):
-  print(dialog.massage)
-  dialog.accept()
-
-
 with sync_playwright() as p:
-    browser=p.chromium.launch(headless=False, channel="chromeS")
+    browser=p.chromium.launch(headless=False, args=["--start-maximized"], slow_mo=100)
 
-    browser=p.chromium.launch(channel='msedge")
-    browser=p.chromium.launch(channel="edge")
-
+    page=browser.new_page(no_viewport=True)
 
 
+    page.goto(
+    "https://www.hyrtutorials.com/p/window-handles-practice.html"
+    )
 
-    page=browser.new_page()
-    page.goto("https://demoqa.com/automation-practice-form")
-
-
-    page.locator("#firstName").type("omkaratanure")
-
-    page.locator("#lastName").fill("atanure")
-    page.get_by_placeholder("name@example.com").fill("omkaratanure111@gmail.com")
-
-    page.locator("input[value='Male']").check()
-
-    page.locator("#userNumber").type("99869856")
-    page.locator("#dateOfBirthInput").click()
+    with page.expect_popup() as newpage:
+        page.get_by_text("Open New Window").click()
 
 
-    page.locator(".react-datepicker__year-select").select_option("1902")
+    new_page=newpage.value
+
+    page.wait_for_load_state()
+
+    selected_option=new_page.locator("#selectnav1").select_option("Tech News")
+
+    print(selected_option)
+
     page.wait_for_timeout(30000)
-    page.locator(".react-datepicker__month-select").select_option("9")
-    page.wait_for_timeout(30000)
-
-    page.locator("[aria-label='Choose Wednesday, June 3rd, 2026']").click()
-
-    page.locator("#hobbies-checkbox-1").check()
-
-    page.locator("#uploadPicture").set_input_files("omkar.pdf")
-
-    page.get_by_text("Alerts, Frame & Windows").click()
-
-    page.get_by_text("Alerts").click()
-    page.locator("#alertButton").click()
-
-    page.on("dialog",handle_dailg)
-
-
-
-    page.locator(".css-hlgwow").fill("Rajasthan")
-
-    page.locator(".css-19bb58m").fill("Jaipur")
-    page.screenshot(path="demoscreeshot.png", full_page=True)
-
-
-
-
-
-
-
